9d401f7398a50c4e16bf9ed898cb80fbf83b6849
Joseph Finney <josephfinney@live.com>
2021-05-20 13:08:41 -0500

    App fully working on single page

---
+++ Client/Ingredient.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace BlazorApp.Client
+{
+    public class Ingredient
+    {
+        public string Name { get; set; } = "";
+        public int Parts { get; set; } = 1;
+
+        public int TotalParts { get; set; }
+
+        public double FinalOunces { get; set; }
+    }
+}
